f57a1501f1c17caa38d55eabb0dc7fce876f31a8
maximilian.zangerl@student.htl-rankweil.at <maximilian.zangerl@student.htl-rankweil.at>
2024-12-10 21:22:14 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,31 +1,29 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import anvil.server
-
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.button_login_click()
     # Any code you write here will run before the form opens.
-  
 
   def button_login_click(self, **event_args):
-    
-      username = self.text_box_1.text
-      password = self.text_box_2.text
+    """Handle login button click"""
+    username = self.text_box_1.text
+    password = self.text_box_2.text
 
-      if self.button_unsafe.selected:
+    # Check which login mode is selected
+    if self.button_unsafe.selected:
         result = anvil.server.call('login_insecure', username, password)
-      elif self.button_safe.selected:
+    elif self.button_safe.selected:
         result = anvil.server.call('login_secure', username, password)
-      else:
+    else:
         result = "Bitte wähle einen Typ aus"
-        
 
-      self.resulttext.text = result
+    # Display the result in resulttext
+    self.resulttext.text = result
 
+  def text_box_2_pressed_enter(self, **event_args):
+    """Handle Enter key in password box"""
+    self.button_login_click()

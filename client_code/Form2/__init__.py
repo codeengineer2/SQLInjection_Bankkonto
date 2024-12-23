0c43d33615430c44ce1f339db64f121e7e36aaf7
maximilian.zangerl@student.htl-rankweil.at <maximilian.zangerl@student.htl-rankweil.at>
2024-12-23 18:02:41 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -10,15 +10,7 @@ class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-  def button_check_click(self, **event_args):
-    # Nutzereingaben aus den Textboxen abholen
-    account_no_input = self.text_box_account_no.text
-    pwd_input = self.text_box_pwd.text
-
-    # Server-Funktion aufrufen, die (unsicher) in die DB schaut
     result = anvil.server.call('check_account', account_no_input, pwd_input)
-
-    # Ergebnis im Label anzeigen
     self.textbalances.text = str(result)
-  
+
+    
